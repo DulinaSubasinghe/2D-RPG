920dd0d258b830079d40499d0866cc982b857e11
Dulina Subasinghe <dulinavsubasinghe7@gmail.com>
2026-04-23 10:02:20 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,158 @@
+class Game {
+  constructor() {
+    this.canvas = document.getElementById('gameCanvas');
+    this.ctx = this.canvas.getContext('2d');
 
+    this.resizeCanvas();
+    window.addEvenListener('resize', () => this.resizeCanvas());
+
+    this.ctx.imageSmoothingEnabled = false;
+
+    this.worldWidth = 5000;
+    this.worldHeight = 5000;
+    this.spawningEnemies = [];
+        
+        this.camera = { x: 0, y: 0, targetX: 0, targetY: 0 };
+        
+        this.player = {
+            x: this.worldWidth / 2,
+            y: this.worldHeight / 2,
+            size: 28,
+            speed: 5,
+            health: 100,
+            maxHealth: 100,
+            invincibleTimer: 0,
+            walkBob: 0
+        };
+        
+        this.currentWeapon = 'mg';
+        
+        this.mgAmmo = {
+            current: 30,
+            total: 120,
+            maxMag: 30,
+            reloading: false,
+            reloadTimer: 0,
+            reloadTime: 167
+        };
+        
+        this.shotgunAmmo = {
+            current: 6,
+            total: 24,
+            maxMag: 6,
+            reloading: false,
+            reloadTimer: 0,
+            reloadTime: 120
+        };
+        
+        this.ammo = this.mgAmmo;
+        
+        this.shooting = false;
+        this.shootCooldown = 0;
+        this.mgShootDelay = 5;
+        this.shotgunShootDelay = 15;
+        this.bullets = [];
+        this.bulletSpeed = 15;
+        this.muzzleFlash = { active: false, timer: 0 };
+        
+        this.molotovs = 3;
+        this.molotovCooldown = 0;
+        this.molotovCooldownMax = 120;
+        this.fireZones = [];
+        this.molotovProjectiles = [];
+        
+        this.damageFlashTimer = 0;
+        this.hitMarkerTimer = 0;
+        this.screenShake = { active: false, intensity: 0, timer: 0 };
+        this.lowAmmoWarningTimer = 0;
+        this.lowHealthWarningTimer = 0;
+        this.recoilAngle = 0;
+        this.recoilTimer = 0;
+        
+        this.fires = [];
+        this.smokeParticles = [];
+        this.fogIntensity = 0.4;
+        
+        this.mouseWorld = { x: 0, y: 0 };
+        this.mouseScreen = { x: 0, y: 0 };
+        this.bloodSplatters = [];
+        
+        this.enemies = [];
+        this.enemyCount = 18;
+        
+        this.trees = [];
+        this.treeCount = 60;
+        this.ponds = [];
+        this.pondCount = 8;
+        this.structures = [];
+        this.ammoPickups = [];
+        this.molotovPickups = [];
+        this.groundMap = [];
+        this.groundSize = 32;
+        this.decorations = [];
+        this.craters = [];
+        this.particles = [];
+        this.lastClickTime = 0;
+        this.knockbackIntensity = 0;
+  
+        this.keys = { w: false, s: false, a: false, d: false };
+        
+        this.wave = 1;
+        this.enemiesToSpawn = 5;  
+        this.enemiesRemaining = 0;
+        this.waveInProgress = false;
+        this.waveCooldown = 0;
+        this.waveMessage = "";
+        this.waveMessageTimer = 0;
+        this.bossSpawned = false;
+        this.waveSpawningComplete = true; // NEW: track if all enemies of current wave have been spawned
+
+        this.paused = false;
+        this.minimapSize = 180;
+        this.minimapZoom = 0.06;
+        this.score = 0;
+        this.kills = 0;
+        this.gameOver = false;
+        
+        // Floating damage numbers
+        this.floatingNumbers = [];
+        
+        // Tutorial
+        this.tutorialSeen = localStorage.getItem('tutorialSeen') === 'true';
+        this.showTutorial = !this.tutorialSeen;
+        
+        this.healthFill = document.getElementById('healthFill');
+        this.healthValue = document.getElementById('healthValue');
+        this.ammoFill = document.getElementById('ammoFill');
+        this.ammoCurrent = document.getElementById('ammoCurrent');
+        this.ammoTotal = document.getElementById('ammoTotal');
+        this.scoreValue = document.getElementById('scoreValue');
+        this.killsValue = document.getElementById('killsValue');
+        this.reloadCircular = document.getElementById('reloadCircular');
+        this.reloadProgressCircle = document.getElementById('reloadProgressCircle');
+        this.lowAmmoWarning = document.getElementById('lowAmmoWarning');
+        this.lowHealthWarning = document.getElementById('lowHealthWarning');
+        this.damageFlash = document.getElementById('damageFlash');
+        this.hitMarker = document.getElementById('hitMarker');
+        this.fogOverlay = document.getElementById('fogOverlay');
+        
+        this.crosshair = document.getElementById('crosshair');
+        this.addCrosshairDot();
+        
+        this.audioContext = null;
+        this.soundsEnabled = true;
+        this.gunshotSound = null;
+        this.reloadSound = null;
+        this.clickSound = null;
+        
+        this.setupInput();
+        this.generateGroundMap();
+        this.generateChaoticWorld();
+        this.generateFiresAndSmoke();
+        this.startWave();
+        this.spawnAmmoPickups();
+        this.spawnMolotovPickups();
+        this.setupAudio();
+        this.updateUI();
+        this.gameLoop();
+    }
